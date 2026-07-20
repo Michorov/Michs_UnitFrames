eb1c049d6ee2e6ca3165576b246e8045a4d6a5d0
Mich <caedesque.apps@icloud.com>
2026-07-20 12:20:26 +0300

    Add header row to pages

--- Options/Pages/General.lua
+++ Options/Pages/General.lua
@@ -14,12 +14,25 @@ function General:Ensure(parent)
 	end
 
 	page = CreateFrame("Frame", nil, parent)
-	page.text = page:CreateFontString(nil, "OVERLAY", "GameFontHighlightSmall")
-	page.text:SetPoint("TOPLEFT", page, "TOPLEFT", 0, 0)
-	page.text:SetText("General page")
+	page.header = CreateFrame("Frame", nil, page)
+	page.header:SetPoint("TOPLEFT", page, "TOPLEFT", 0, 0)
+	page.header:SetPoint("TOPRIGHT", page, "TOPRIGHT", 0, 0)
+
+	page.body = CreateFrame("Frame", nil, page)
+	page.body:SetPoint("BOTTOMLEFT", page, "BOTTOMLEFT", 0, 0)
+	page.body:SetPoint("BOTTOMRIGHT", page, "BOTTOMRIGHT", 0, 0)
+
+	page.header.title = page.header:CreateFontString(nil, "OVERLAY", "GameFontHighlightSmall")
+	page.header.title:SetPoint("LEFT", page.header, "LEFT", 0, 0)
+	page.header.title:SetJustifyH("LEFT")
+	page.header.title:SetTextColor(0.88, 0.89, 0.92, 1)
+	page.header.title:SetText("General")
 
 	function page:UpdateLayout()
-		self.text:SetFont("Fonts\\ARIALN.TTF", PP:ScaleFont(16), "")
+		self.header:SetHeight(PP:ToUIScaled(32))
+		self.header.title:SetFont("Fonts\\ARIALN.TTF", PP:ScaleFont(20), "")
+		self.body:SetPoint("TOPLEFT", self.header, "BOTTOMLEFT", 0, PP:ToUIScaled(-16))
+		self.body:SetPoint("TOPRIGHT", self.header, "BOTTOMRIGHT", 0, PP:ToUIScaled(-16))
 	end
 
 	return page
